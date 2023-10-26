095d20733bb9c199f0acb2b62b47e8b6b87c3da1
Fabiana Campanari <fabicampanari@gmail.com>
2023-10-26 04:08:39 -0300

    sumbot.rb
    added sumbot.rb
    
    Signed-off-by: Fabiana Campanari <fabicampanari@gmail.com>

---
+++ sumbot.rb
@@ -0,0 +1,44 @@
+puts "<h1>SumBot - Free Code</h1>
+
+<h3>AI-Powered ChatBot with GPT integration.</h3>
+
+<p>This is a project that leverages Artificial Intelligence (AI) in the business sector. It was created specifically for the Project and Startup Competition promoted by the International Business School Americas (IBS Americas). This initiative is a collaborative effort with various universities and foundations, both public and private, from Europe and the United States. The project reveals the activities related to the competition, which centers on projects and startups utilizing AI.</p>
+
+<h2>Why SumBot?</h2>
+
+<p>In today's world of information overload, we are often bombarded with lengthy texts and messages. What if there was a solution to this problem that didn't require programming skills? 
+We introduce SumBot, a tool that automates the task of summarizing long messages for you. And the best part is, you don't need to be a programmer to use it. This project utilizes three essential tools: the OpenAI API, Slack (where our bot resides), and Zapier for integration.</p>
+
+<h2>How It Works:</h2>
+
+<ol>
+  <li><strong>OpenAI API:</strong> This is the brain of our bot. We will use the OpenAI API to harness the power of the GPT model, an artificial intelligence that excels at understanding and generating text.</li>
+  <li><strong>Slack:</strong> This is the environment where our bot will reside. Slack is a communication platform that enables the creation and use of bots.</li>
+  <li><strong>Zapier:</strong> This is the tool that will integrate Slack with the OpenAI API. Zapier is an automation tool that allows you to create workflows between different platforms.</li>
+</ol>
+
+<h2>Who Is It For?</h2>
+
+<ul>
+  <li><span>:briefcase:</span> <strong>Business Professionals:</strong> Managers and executives who need to keep up with a large volume of business communications and reports. SumBot can help them efficiently extract key information from extensive documents.</li>
+  <li><span>:mortar_board:</span> <strong>Students:</strong> Students who want to summarize lengthy texts, academic articles, or study documents to save time when reviewing content.</li>
+  <li><span>:telephone_receiver:</span> <strong>Customer Support Teams:</strong> Support agents who need to quickly understand customer concerns in lengthy conversations. SumBot can help summarize customer interactions.</li>
+  <li><span>:mag:</span> <strong>Curious Individuals:</strong> Anyone who wants to stay updated on the latest news or topics of interest but doesn't have time to read extensive articles.</li>
+  <li><span>:microscope:</span> <strong>Researchers:</strong> Research professionals who want to summarize scientific papers or reports to identify relevant information for their research.</li>
+  <li><span>:mega:</span> <strong>Marketing Professionals:</strong> Marketers who want to monitor what is being said about their brand or products on social media and online forums.</li>
+  <li><span>:school:</span> <strong>Educators:</strong> Teachers who want to simplify lengthy texts to make them more accessible to students.</li>
+  <li><span>:pencil2:</span> <strong>Writers and Authors:</strong> Writers who want to extract key information from research and reference documents.</li>
+</ul>
+
+<p>These are just a few examples; SumBot has the potential to be useful in many other scenarios!</p>
+
+<h2>Advantages of SumBot:</h2>
+
+<p>The use of SumBot or a similar chatbot employing Artificial Intelligence to summarize messages or information can offer several advantages, including:</p>
+
+<ol>
+  <li><span>:hourglass:</span> <strong>Time Savings:</strong> One of the most significant advantages is time savings. SumBot can quickly analyze and summarize extensive messages or documents, saving users the effort of reading and comprehending the complete content.</li>
+  <li><span>:mag_right:</span> <strong>Quick Access to Relevant Information:</strong> Helps users access relevant and critical information without the need to go through large volumes of text.</li>
+  <li><span>:rocket:</span> <strong>Improved Productivity:</strong> The ability to obtain information summaries helps improve productivity, as users can focus on the most important and relevant content.</li>
+  <li><span>:inbox_tray:</span> <strong>Information Overload Management:</strong> In a world with constant information overload, SumBot provides a solution for managing and filtering information, making it more manageable.</li>
+</ol>"
